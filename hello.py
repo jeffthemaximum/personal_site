45db7ec44796a117bf6fb463236bcaabf3ea940f
jeffthemaximum <frey.maxim@gmail.com>
2015-12-15 11:21:56 -0500

    ialcs survey

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from random import randint
 
 app = Flask(__name__)
 
@@ -16,6 +17,12 @@ def index():
 @app.route('/byte')
 def byte():
     return render_template('byte.html')
+
+
+@app.route('/survey')
+def survey():
+    i = str(randint(1,6))
+    return render_template('survey.html', i=i)
 
 
 if __name__ == '__main__':
